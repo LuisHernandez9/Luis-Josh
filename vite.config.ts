3fce932a27fd2da406bf7abfe777c527d00cd4d9
Luis <134240596+LuisHernandez9@users.noreply.github.com>
2025-08-18 22:09:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Luis-Josh/', // e.g. '/eventpulse-nc-site/'
-})
+  // IMPORTANT: set to your exact repo name between slashes
+  // Example: '/Luis-Josh/'
+  base: "/Luis-Josh/"
+});
