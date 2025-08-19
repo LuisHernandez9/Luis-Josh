879b52995eb180aa84a6c4e220eba042b604c0da
Luis <134240596+LuisHernandez9@users.noreply.github.com>
2025-08-18 22:56:01 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: set to your exact repo name between slashes
-  // Example: '/Luis-Josh/'
-  base: "/Luis-Josh/"
+  base: "/Luis-Josh/" // repo name (case-sensitive)
 });
