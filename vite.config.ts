e06709d2579829b714e98d228c35fb5f3e61dbaf
Luis <134240596+LuisHernandez9@users.noreply.github.com>
2025-09-10 10:28:22 -0400

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Luis-Josh/" // repo name (case-sensitive)
+  base: "/EventPulse_NC/" // repo name (case-sensitive)
 });
